055dac53c02ec4357f39b8da92b7109900e1fd6f
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-06-21 14:45:20 -0700

    Update Classes.hx

--- src/cxxcompiler/subcompilers/Classes.hx
+++ src/cxxcompiler/subcompilers/Classes.hx
@@ -1028,7 +1028,7 @@ class Classes extends SubCompiler {
 						for(ex in exprs) {
 								// trace($type(ex));
 								switch(ex.expr) {
-										case TBinop(OpAssign, {expr: TField({expr: TConst(TThis) | TLocal(TVar) }, name)}, e2): {
+										case TBinop(OpAssign, {expr: TField({expr: TConst(TThis)}, name)}, e2) | TBinop(OpAssign, {expr: TField({expr: TConst(TThis)}, name)}, e2): {
 										switch(e2.expr) {
 										    case TConst(_): constructorExpressions.push(e2);
 										    case _: cleanExpressions.push(ex);
