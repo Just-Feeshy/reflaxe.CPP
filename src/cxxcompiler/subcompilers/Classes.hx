c28f2df59793e4db036836ba8d8221c6d6e9b99a
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-06-23 09:15:18 -0700

    Update Classes.hx

--- src/cxxcompiler/subcompilers/Classes.hx
+++ src/cxxcompiler/subcompilers/Classes.hx
@@ -1047,7 +1047,7 @@ class Classes extends SubCompiler {
 														    case _: "0";
 														}
 													}
-												    case _: { trace(e2.expr); }
+												    case _: { trace(e2.expr); "0"; }
 												}
 
 												constructorInitFields.push(name_raw + "(" + value_raw + ")");
